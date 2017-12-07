5c5fa15e34b6e72c5b124ceef9313da7b6a028f8
karlloic <karlloic@gmail.com>
2017-12-06 22:15:59 -0500

    corrected AUC

--- R/modelEvalAUC.R
+++ R/modelEvalAUC.R
@@ -1,9 +1,9 @@
-modelEvalAUC <- function(predicted, actual)
+modelEvalAUC <- function(actual, predicted)
 {
   # Functiont to calculate AUC
   library(pROC)
   
-  roc_obj <- roc(predicted, actual)
+  roc_obj <- roc(actual, predicted)
   score <- auc(roc_obj)
   
   return(score)
